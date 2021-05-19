2da07637628c0e229df53c9454e4b56d0dcf5c3a
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-05-19 08:20:02 -0500

    Basic gatsby project setup

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Footer = () => {
+  return (
+    <footer>
+      <h2>Footer FTW! </h2>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+const Contact = () => {
+  return (
+    <Layout>
+      <h1>Contact Orson</h1>
+      <p>nobody contacts Orson...</p>
+      <p>... Orson contacts you!</p>
+    </Layout>
+  )
+}
+
+export default Contact
