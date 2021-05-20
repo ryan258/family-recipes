75ad542842f42e5cb2f26ad56fb6aa5036913b78
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-05-20 09:15:42 -0500

    Apply a different view of RecipesList on /about and /contact

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,22 @@
 import React from "react"
 import Layout from "../components/Layout"
 
-import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import { Link, graphql } from "gatsby"
+import RecipesList from "../components/RecipesList"
 
-const About = () => {
+const About = ({
+  data: {
+    allContentfulRecipe: { nodes: recipes },
+  },
+}) => {
+  // console.log(data)
   return (
     <Layout>
       <main className="page">
         <section className="about-page">
           <article>
-            <h2>I'm baby coloring book poke taxidermy</h2>
+            <h2>I'm critter coloring book poke taxidermy</h2>
             <p>
               Taxidermy forage glossier letterpress heirloom before they sold
               out you probably haven't heard of them banh mi biodiesel chia.
@@ -30,9 +36,32 @@ const About = () => {
             placeholder="blurred"
           />
         </section>
+        <section className="featured-recipes">
+          <h5>Check Out These Recipes!</h5>
+          <RecipesList recipes={recipes} />
+        </section>
       </main>
     </Layout>
   )
 }
+
+export const query = graphql`
+  {
+    allContentfulRecipe(
+      sort: { fields: title, order: ASC }
+      filter: { featured: { eq: true } }
+    ) {
+      nodes {
+        id
+        title
+        prepTime
+        cookTime
+        image {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED)
+        }
+      }
+    }
+  }
+`
 
 export default About
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,11 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { Link, graphql } from "gatsby"
+import RecipesList from "../components/RecipesList"
 
-const Contact = () => {
+const Contact = ({ data }) => {
+  const recipes = data.allContentfulRecipe.nodes
+
   return (
     <Layout>
       <main className="page">
@@ -38,9 +42,32 @@ const Contact = () => {
             </form>
           </article>
         </section>
+        <section className="featured-recipes">
+          <h5>Check Out These Recipes!</h5>
+          <RecipesList recipes={recipes} />
+        </section>
       </main>
     </Layout>
   )
 }
+
+export const query = graphql`
+  {
+    allContentfulRecipe(
+      sort: { fields: title, order: ASC }
+      filter: { featured: { eq: true } }
+    ) {
+      nodes {
+        id
+        title
+        prepTime
+        cookTime
+        image {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED)
+        }
+      }
+    }
+  }
+`
 
 export default Contact
